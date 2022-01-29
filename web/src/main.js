efa8d4bdd27277f01da06a2cff4711aaa3d29b26
lizhongwen <1829664373@qq.com>
2022-01-29 12:25:11 +0800

    init

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import register from './common/register.vue'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(register).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
