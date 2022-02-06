d46ccf58754d4244ad7cc301aa493d56d17606d4
lizhongwen <1829664373@qq.com>
2022-02-06 22:08:58 +0800

    element

--- web/src/main.js
+++ web/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import Axios from 'axios'
+
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios
 Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
